257246edfa5f08a4037202978c5ab64e4b2439d1
Keshav KK <kumar-ke@ibjapan.jp>
2025-08-13 08:50:57 +0900

    SHHログインアラートwithWebHook

--- notify_teams_ec2_login.sh
+++ notify_teams_ec2_login.sh
@@ -76,5 +76,5 @@ curl -s -H "Content-Type: application/json" -d "$PAYLOAD" "$WEBHOOK_URL"
 # sudo vi /usr/local/bin/notify_teams_ec2_login.sh
 # sudo chmod +x /usr/local/bin/notify_teams.sh
 # sudo vi /etc/pam.d/sshd
-# session optional pam_exec.so /usr/local/bin/notify_teams.sh login | session required pam_exec.so /usr/local/bin/notify_teams.sh login
+# session optional pam_exec.so /usr/local/bin/notify_teams_ec2_login.sh login | session required pam_exec.so /usr/local/bin/notify_teams_ec2_login.sh login
 # sudo systemctl restart sshd
